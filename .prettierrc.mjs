7534503acfd4bd30aba076b1c5cb7a87e66a680a
David Inoa <davidinoa@gmail.com>
2023-12-09 12:44:55 -0500

    chore: initialize project with Astro

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  semi: false,
+  singleQuote: true,
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+}
